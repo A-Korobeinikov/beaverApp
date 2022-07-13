4b40c1b1a234f25a54088047ff11d8ad6206eddb
Anton Korobeinikov <an.korobeinikov@mail.ru>
2022-07-13 13:43:50 +0300

    feat: setting linters

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+
+import { App } from './components/app';
+import { store } from './store/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -14,10 +13,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
